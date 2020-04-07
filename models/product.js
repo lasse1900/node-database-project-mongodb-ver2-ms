438792913b34a2e7600422e2f7e500ece5ad4a0b
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-07 12:28:48 +0300

    code refactoring

--- models/product.js
+++ models/product.js
@@ -1,22 +1,30 @@
 const fs = require('fs')
 const path = require('path')
 
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+
 module.exports = class Product {
   constructor(t) {
     this.title = t
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = []
-      if (!err) {
-        products = JSON.parse(fileContent)
-      }
+    getProductsFromFile(products => {
       products.push(this)
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err)
@@ -25,17 +33,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        rcb([])
-      }
-      cb(JSON.parse(fileContent))
-    })
+    getProductsFromFile(cb)
   }
-
 }
