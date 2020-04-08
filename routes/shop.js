65a03f332a52c41742260436575abade4c1eff0c
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-08 13:03:20 +0300

    adding Cart model to file

--- routes/shop.js
+++ routes/shop.js
@@ -14,7 +14,7 @@ router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart)
 
-router.get('/cart', shopController.postCart)
+router.post('/cart', shopController.postCart);
 
 router.get('/orders', shopController.getOrders)
 
