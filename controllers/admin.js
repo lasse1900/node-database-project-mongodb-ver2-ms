d5331905a49e2dff84b42a6721d394af61f0dd4f
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-08 16:07:29 +0300

    linking to edit page

--- controllers/admin.js
+++ controllers/admin.js
@@ -37,6 +37,9 @@ exports.getEditProduct = (req, res, next) => {
   })
 };
 
+exports.postEditProduct = (req, res, next) => {
+
+};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
