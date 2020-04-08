f7d1f05d2ac6583fdb5a7d9a07642276ee44b065
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-08 17:04:34 +0300

    editing product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,19 +13,19 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl
   const description = req.body.description
   const price = req.body.price
-  const product = new Product(title, imageUrl, description, price)
+  const product = new Product(null, title, imageUrl, description, price)
   product.save()
   res.redirect('/');
 };
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit
-  if(!editMode) {
+  if (!editMode) {
     return res.redirect('/')
   }
   const prodId = req.params.productId
   Product.findById(prodId, product => {
-    if(!product) {
+    if (!product) {
       return res.redirect('/')
     }
     res.render('admin/edit-product', {
@@ -38,7 +38,20 @@ exports.getEditProduct = (req, res, next) => {
 };
 
 exports.postEditProduct = (req, res, next) => {
-
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
