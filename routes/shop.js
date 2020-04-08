602f91d54934415a50a4d8f0e704bbbd5e4276e1
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-08 11:56:36 +0300

    passing data with POST requests

--- routes/shop.js
+++ routes/shop.js
@@ -14,6 +14,8 @@ router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart)
 
+router.get('/cart', shopController.postCart)
+
 router.get('/orders', shopController.getOrders)
 
 router.get('/checkout', shopController.getCheckout)
