cf3f95fbf951154b0fc0fb4763c172441bd5d2a6
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-10 11:36:10 +0300

    fetch admin products

--- controllers/admin.js
+++ controllers/admin.js
@@ -64,13 +64,15 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin products',
-      path: '/admin/products',
-    });
-  })
+  Product.findAll()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+  .catch(err => console.log(err))
 };
 
 exports.postDeleteProduct = (req, res, next) => {
