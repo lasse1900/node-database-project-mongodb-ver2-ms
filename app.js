78068560e1ff2ceed5886f0158948234e2fea68b
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-10 12:55:28 +0300

    created a test user

--- app.js
+++ app.js
@@ -19,18 +19,36 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findByPk(1).then(user => {
+    req.user = user
+  })
+    .catch(err => console.log(err))
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404)
 
-Product.belongsTo(User, {constrains: true, onDelete: 'CASCADE'})
-User.hasMany(Product)
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
 
 sequelize
-  .sync({force: true})  // only in development
+  // .sync({ force: true })
+  .sync()
   .then(result => {
+    return User.findByPk(1);
     // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Lasse', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch(err => {
