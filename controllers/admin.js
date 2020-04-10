1f8565763197de44804ad53a5cc5ece4dbc0a005
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-10 11:51:23 +0300

    update products

--- controllers/admin.js
+++ controllers/admin.js
@@ -33,17 +33,22 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/')
   }
   const prodId = req.params.productId
-  Product.findByPk(prodId, product => {
-    if (!product) {
-      return res.redirect('/')
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-    });
-  })
+  Product.findByPk(prodId)
+    .then(product => {
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -52,15 +57,21 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+    .then(product => {
+      product.title = updatedTitle
+      product.price = updatedPrice
+      product.imageUrl = updatedImageUrl
+      product.description = updatedDesc
+      return product.save()
+    })
+    .then(result => {
+      console.log('updated product')
+      res.redirect('/admin/products');
+    })
+    .catch(err => {
+      console.log(err)
+    })
 };
 
 exports.getProducts = (req, res, next) => {
@@ -72,7 +83,7 @@ exports.getProducts = (req, res, next) => {
         path: '/admin/products'
       });
     })
-  .catch(err => console.log(err))
+    .catch(err => console.log(err))
 };
 
 exports.postDeleteProduct = (req, res, next) => {
