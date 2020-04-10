5a9664e43d1fdbc917f02df3c3d8d48c6dd9e15f
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-10 12:00:49 +0300

    added delete product

--- controllers/admin.js
+++ controllers/admin.js
@@ -21,6 +21,7 @@ exports.postAddProduct = (req, res, next) => {
   }).then(result => {
     // console.log(result)
     console.log('Created product')
+    res.redirect('/admin/products')
   })
     .catch(err => {
       console.log(err)
@@ -88,6 +89,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+    .then(product => {
+      return product.destroy()
+    })
+    .then(result => {
+      console.log('destroyed product')
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err))
 };
