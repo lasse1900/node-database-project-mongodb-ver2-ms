3e3af41b8b3d36a7a85ba884e45234fe4f225ade
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-10 11:24:38 +0300

    using where clause in getProduct()

--- controllers/shop.js
+++ controllers/shop.js
@@ -17,10 +17,19 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
+  // Product.findAll({ where: { id: prodId } })
+  //   .then(products => {
+  //     res.render('shop/product-detail', {
+  //       product: products[0],
+  //       pageTitle: products[0].title,
+  //       path: '/products'
+  //     });
+  //   })
+  // .catch(err => console.log(err))
   Product.findByPk(prodId)
-    .then(([product]) => {
+    .then((product) => {
       res.render('shop/product-detail', {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: '/products'
       });
