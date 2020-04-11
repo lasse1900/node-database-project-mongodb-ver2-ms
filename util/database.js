05a19d159600468d9a356877146c2ade1cda5bdc
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-11 14:57:49 +0300

    connected to MongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
-const Sequelize = require('sequelize')
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node-complete', 'root', 'lasselasse', {
-  dialect: 'mysql',
-  host: 'localhost'
-})
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://academic:paarynapuu@cluster0-itz0t.mongodb.net/test?retryWrites=true&w=majority')
+    .then(client => {
+      console.log('Connected')
+      callback(client)
+    })
+    .catch(err => console.log(err))
+}
 
-module.exports = sequelize
+module.exports = mongoConnect
