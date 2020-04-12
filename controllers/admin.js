f730fdd3713f7c682d12e6af1b84d0a4f890a5d0
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-12 12:39:17 +0300

    fixed add product func - but edit product fails

--- controllers/admin.js
+++ controllers/admin.js
@@ -32,7 +32,7 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/')
   }
   const prodId = req.params.productId
-  Product.findById(prodId)
+  Product.findByPk(prodId)
     .then(product => {
       if (!product) {
         return res.redirect('/');
@@ -88,11 +88,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId)
-    .then(product => {
-      return product.destroy()
-    })
-    .then(result => {
+  Product.deleteById(prodId)
+    .then(() => {
       console.log('destroyed product')
       res.redirect('/admin/products');
     })
