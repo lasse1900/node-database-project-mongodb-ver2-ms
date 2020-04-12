28336918e069c0ca3cda8545dfbf9740bedccfd4
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-12 15:19:45 +0300

    store the user to db

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect
+const User = require('./models/user')
 
 const app = express();
 
@@ -18,12 +19,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findByPk(1).then(user => {
-  //   req.user = user
-  //   next()
-  // })
-  //   .catch(err => console.log(err))
-  next()
+  User.findById('5e9303875a435b08c917db3a').then(user => {
+    req.user = user
+    next()
+  })
+    .catch(err => console.log(err))
 })
 
 app.use('/admin', adminRoutes);
