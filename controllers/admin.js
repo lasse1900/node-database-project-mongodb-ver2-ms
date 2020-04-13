88091ab7864b2b8a58a3b8d807043948db729111
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-13 16:10:31 +0300

    select and populate example

--- controllers/admin.js
+++ controllers/admin.js
@@ -80,7 +80,10 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+    // .select('title price -_id')
+    // .populate('userId', 'name')
     .then(products => {
+      console.log(products)
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
