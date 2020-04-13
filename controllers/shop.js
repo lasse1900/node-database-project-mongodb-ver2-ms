0ea6e1985a7f4e25158ba69c7359343896361ba9
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-13 10:43:49 +0300

    store multiple products to cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -61,11 +61,12 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId).then(product => {
-    return req.user.addToCart(product)
-  }).then(result => {
-    console.log(result)
-  })
+  Product.findById(prodId)
+    .then(product => {
+      return req.user.addToCart(product)
+    }).then(result => {
+      console.log(result)
+    })
   // let fetchedCart;
   // let newQuantity = 1;
   // req.user
