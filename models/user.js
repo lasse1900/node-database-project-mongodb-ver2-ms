6e0a98f68409f38fb6ccba27476203837ad190e8
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-13 11:55:13 +0300

    add order data to db

--- models/user.js
+++ models/user.js
@@ -84,9 +84,17 @@ class User {
 
   addOrder() {
     const db = getDb()
-    return db
-      .collection('orders')
-      .insertOne(this.cart)
+    return this.getCart()
+      .then(products => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name
+          }
+        }
+        return db.collection('orders').insertOne(order)
+      })
       .then(result => {
         this.cart = { items: [] }
         return db
@@ -95,6 +103,11 @@ class User {
             { _id: new ObjectId(this._id) },
             { $set: { cart: { items: [] } } })
       })
+  }
+
+  getOrders() {
+    const db = getDb()
+    // return db.collection('orders')
   }
 
   static findById(userId) {
