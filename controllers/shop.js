7a91daf50ffb280bf61655b26303cbcbc47f9e1d
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-13 12:17:25 +0300

    display orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -77,7 +77,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.getOrders = (req, res, next) => {
   req.user
-    .getOrders({ include: ['products'] })
+    .getOrders()
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
--- models/user.js
+++ models/user.js
@@ -107,7 +107,8 @@ class User {
 
   getOrders() {
     const db = getDb()
-    // return db.collection('orders')
+    return db.collection('orders').find({'user._id': new ObjectId(this._id)})
+    .toArray()
   }
 
   static findById(userId) {
