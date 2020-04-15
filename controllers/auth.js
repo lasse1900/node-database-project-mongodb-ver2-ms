442441052dd1084d4fb38e2fdd07c558c6708354
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-15 13:01:25 +0300

    using session middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 exports.getLogin = (req, res, next) => {
   // console.log(req.get('Cookie').split(':')[0].trim().split('=')[1])
   // const isLoggedIn = req.get('Cookie').split(':')[0].trim().split('=')[1]
+  console.log(req.session.isLoggedIn)
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -9,6 +10,7 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly')
+  // res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly')
+  req.session.isLoggedIn = true
   res.redirect('/')
 };
