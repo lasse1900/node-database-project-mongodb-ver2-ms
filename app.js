44df536b721a05bec127122bc522c7bb40ba7d8a
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-16 10:35:25 +0300

    authetication flow implemented

--- app.js
+++ app.js
@@ -58,18 +58,6 @@ mongoose
     MONGODB_URI
   )
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Lasse',
-          email: 'lasse@example.com',
-          cart: {
-            items: []
-          }
-        })
-        user.save()
-      }
-    })
     app.listen(3000)
   })
   .catch(err => {
