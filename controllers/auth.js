7d708c39c2f430083a8aaa426b8bc764636f7dd9
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-20 12:18:23 +0300

    email send with google less secure apps

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,20 @@
-const bcrypt = require('bcryptjs')
-const User = require('../models/user')
+require('dotenv').config();
+const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const log = console.log;
+
+const User = require('../models/user');
+
+// Step 1
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL || 'abc@gmail.com', // TODO: your gmail account 
+    pass: process.env.PASSWORD || '1234' // TODO: your gmail password
+  }
+});
 
 exports.getLogin = (req, res, next) => {
-  // console.log(req.flash('error')) 
-  // console.log(req.session.isLoggedIn)
-
   let message = req.flash('error')
   if (message.length > 0) {
     message = message[0]
@@ -62,32 +72,65 @@ exports.postLogin = (req, res, next) => {
     .catch(err => console.log(err))
 };
 
+// Step 2
+let mailOptions = {
+  from: 'lkyttl@gmail.com', // TODO: email sender
+  to: 'lauri.kyttala@gmail.com', // TODO: email receiver
+  subject: 'Nodemailer - Test',
+  html: '<h1>Hey this works!!</h1>Here you can reset you password'
+};
+
 exports.postSignup = (req, res, next) => {
-  const email = req.body.email
-  const password = req.body.password
-  const confirmedPassword = req.body.confirmedPassword
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
-        req.flash('error', 'Email already taken')
-        return res.redirect('/signup')
+        req.flash(
+          'error',
+          'E-Mail exists already, please pick a different one.'
+        );
+        return res.redirect('/signup');
       }
-      return bcrypt.hash(password, 12)
+      return bcrypt
+        .hash(password, 12)
         .then(hashedPassword => {
           const user = new User({
             email: email,
             password: hashedPassword,
             cart: { items: [] }
-          })
-          return user.save()
+          });
+          return user.save();
         })
         .then(result => {
-          res.redirect('/login')
+          res.redirect('/login');
+          return transporter.sendMail(
+            {
+              to: req.body.email,
+              from: 'shop@node-complete.com',
+              subject: 'Password reset',
+              html: `
+                <p>You requested a password reset</p>
+                <p>Click this <a href="http://localhost:3000/reset/token">link</a> to set a new password.</p>
+              `
+            },
+            (err, data) => {
+              if (err) {
+                console.log('Error occurs', err)
+                return log('Error occurs', err);
+              }
+              console.log('Email sent!')
+              return log('Email sent!');
+            });
         })
+        .catch(err => {
+          console.log(err);
+        });
     })
     .catch(err => {
-      console.log(err)
-    })
+      console.log(err);
+    });
 };
 
 exports.postLogout = (req, res, next) => {
