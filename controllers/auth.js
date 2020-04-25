28bcc2a5897ca8cf5fd1ac73dc2d17b324f50f73
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-25 12:05:35 +0300

    added isEmail validator with express-validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require('crypto')
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const mailGun = require('nodemailer-mailgun-transport');
+const { validationResult } = require('express-validator/check')
 const log = console.log;
 
 const User = require('../models/user');
@@ -80,6 +81,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
+    });
+  }
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
