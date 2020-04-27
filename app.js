d2b1c76116b43570e6490afc01c2278c01825a56
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-27 11:20:03 +0300

    using error handling middleware corrently, beaware of infinite loops - use next()

--- app.js
+++ app.js
@@ -43,27 +43,28 @@ app.use(csrfProtection);
 app.use(flash())
 
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  // throw new Error('Sync Dummy')
   if (!req.session.user) {
     return next()
   }
   User.findById(req.session.user._id)
     .then(user => {
-      if(!user) {
+      if (!user) {
         return next()
       }
       req.user = user
       next()
     })
     .catch(err => {
-      throw new Error(err)
+      next(new Error(err))
     })
 })
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
-});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -74,8 +75,12 @@ app.get('/500', errorController.get500)
 app.use(errorController.get404)
 
 app.use((error, req, res, next) => {
-  
-  res.redirect('/500')
+  // res.redirect('/500')
+  res.status(500).render('500', {
+    pageTitle: 'Some error occurred!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  })
 })
 
 mongoose
